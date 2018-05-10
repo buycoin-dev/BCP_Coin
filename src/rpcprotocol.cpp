e0338d5f2fb3c0b3214ebb7d365b5009b3862f09
Vlad Sh <buycoin-dev@users.noreply.github.com>
2018-05-10 21:53:59 +0600

    Add files via upload

--- src/rpcprotocol.cpp
+++ src/rpcprotocol.cpp
@@ -37,7 +37,7 @@ string HTTPPost(const string& strMsg, const map<string,string>& mapRequestHeader
 {
     ostringstream s;
     s << "POST / HTTP/1.1\r\n"
-      << "User-Agent: BCPcoin-json-rpc/" << FormatFullVersion() << "\r\n"
+      << "User-Agent: BuyCoin-json-rpc/" << FormatFullVersion() << "\r\n"
       << "Host: 127.0.0.1\r\n"
       << "Content-Type: application/json\r\n"
       << "Content-Length: " << strMsg.size() << "\r\n"
@@ -60,7 +60,7 @@ string HTTPReply(int nStatus, const string& strMsg, bool keepalive)
     if (nStatus == HTTP_UNAUTHORIZED)
         return strprintf("HTTP/1.0 401 Authorization Required\r\n"
             "Date: %s\r\n"
-            "Server: BCPcoin-json-rpc/%s\r\n"
+            "Server: BuyCoin-json-rpc/%s\r\n"
             "WWW-Authenticate: Basic realm=\"jsonrpc\"\r\n"
             "Content-Type: text/html\r\n"
             "Content-Length: 296\r\n"
@@ -85,9 +85,9 @@ string HTTPReply(int nStatus, const string& strMsg, bool keepalive)
             "HTTP/1.1 %d %s\r\n"
             "Date: %s\r\n"
             "Connection: %s\r\n"
-            "Content-Length: %"PRIszu"\r\n"
+            "Content-Length: %u\r\n"
             "Content-Type: application/json\r\n"
-            "Server: BCPcoin-json-rpc/%s\r\n"
+            "Server: BuyCoin-json-rpc/%s\r\n"
             "\r\n"
             "%s",
         nStatus,
